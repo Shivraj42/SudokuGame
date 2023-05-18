7ef7278ef765fd431c96157e2f5da37dce13c064
LEGION <LEGION@DESKTOP-FK19AM2>
2023-05-18 10:42:41 +0530

    first comit

---
+++ src/main/java/com/mycompany/sudoku_solver/Sudoku_Solver.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sudoku_solver;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Sudoku_Solver {
+    public static void main(String [] args){
+        SudokuFrame f= new SudokuFrame();
+    }
+}
